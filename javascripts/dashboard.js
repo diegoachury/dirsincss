907acc6acd4bc34614a75927520f99f072935de2
Diegoachury <achurycompany@gmail.com>
2014-11-17 08:53:07 -0500

    generando estilo javascript

---
+++ javascripts/dashboard.js
@@ -0,0 +1,21 @@
+var section = document.getElementById('hero');
+var boxbg = document.getElementById('box-bg');
+section.style.background = 'url(../img/bg.JPG)';
+section.style.backgroundSize = "cover";
+section.style.backgroundPosition = "center";
+section.style.backgroundRepeat = "no-repeat";
+section.style.position = "absolute";
+section.style.top = "0";
+section.style.bottom = "0";
+section.style.left = "0";
+section.style.right = "0";
+section.style.zIndex = "10";
+
+//box-bg
+boxbg.style.backgroundColor = 'rgba(243, 137, 35, 0.6)';
+boxbg.style.position = "absolute";
+boxbg.style.zIndex = '11';
+boxbg.style.top = "0";
+boxbg.style.bottom = "0";
+boxbg.style.left = "0";
+boxbg.style.right = "0";
